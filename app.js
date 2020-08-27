3aa775bbdfd4f8640e3f5be0adb6a68045d6768b
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-08-27 14:21:39 +0100

    Created function that constructs a target URL and validates query prior to scraping

--- app.js
+++ app.js
@@ -8,7 +8,18 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.route('/properties')
+  .get((req, res) => {
+    // SAMPLE: location=harrow&low=30&high=200000&terraced=true&radius=2
+    scrape.constructURL(req.query);
+    res.send(req.query);
+  });
+
 (async () => {
-  const properties = await scrape.scrapeProperties('https://www.home.co.uk/search/results.htm?location=harrow&TOWN_SEARCH=1&high=500000');
-  console.log(properties.length);
+  //const properties = await scrape.scrapeProperties('https://www.home.co.uk/search/results.htm?location=harrow&TOWN_SEARCH=1&high=500000');
+  //console.log(properties.length);
 })();
+
+app.listen(port, () => {
+  console.log(`UK Property API listening on port ${port}`);
+});
--- js/scrape.js
+++ js/scrape.js
@@ -1,5 +1,24 @@
 const puppeteer = require('puppeteer');
 
+exports.constructURL = (query) => {
+  const baseURL = 'https://www.home.co.uk/search/results.htm?'
+
+  const keys = Object.keys(query);
+  const validKeys = ['low', 'high', 'location', 'radius', 'minbeds', 'maxbeds', 'detached',
+    'terraced', 'flat', 'semi'];
+
+  // Exclude sold properties and given there is no browser do not show showmap
+  let cleanQuery = 'inc_sold=0&showmap=0';
+
+  keys.forEach((key, index) => {
+    if (validKeys.includes(key.toLowerCase())) {
+      cleanQuery += `&${key}=${query[key]}`;
+    }
+  });
+
+  console.log(cleanQuery)
+};
+
 exports.scrapeProperties = async (url) => {
   const properties = [];
   const browser = await puppeteer.launch();
