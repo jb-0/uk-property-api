9ea74652983aeed14a50c00ea86874b0122af2e3
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-08-27 08:56:57 +0100

    Scraping complete for core details of property, these are added to a property object

--- app.js
+++ app.js
@@ -8,19 +8,40 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// https://www.home.co.uk/search/
-
 async function scrapeProperties(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
-  const [el] = await page.$x('//*[@id="homeco_v6_main"]/div[1]/div[2]/div[3]/table/tbody/tr[1]/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td[2]/b/a')
-  const propertyName = await (await el.getProperty('textContent')).jsonValue();
+  // Get property name
+  let [el] = await page.$x('//*[@id="homeco_v6_main"]/div[1]/div[2]/div[3]/table/tbody/tr[1]/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td[2]/b/a');
+  let propertyName = await (await el.getProperty('textContent')).jsonValue();
+  propertyName = propertyName.replace('\n', ' ');
 
+  // Get property link
+  const propertyLink = await (await el.getProperty('href')).jsonValue();
 
-  console.log(propertyName)
+  // Get property type
+  [el] = await page.$x('//*[@id="homeco_v6_main"]/div[1]/div[2]/div[3]/table/tbody/tr[11]/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td[3]');
+  const propertyType = await (await el.getProperty('textContent')).jsonValue();
 
+  // Get price
+  [el] = await page.$x('//*[@id="homeco_v6_main"]/div[1]/div[2]/div[3]/table/tbody/tr[1]/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td[1]/b');
+  const propertyPrice = await (await el.getProperty('textContent')).jsonValue();
+
+  // Get image src
+  [el] = await page.$x('//*[@id="homeco_v6_main"]/div[1]/div[2]/div[3]/table/tbody/tr[1]/td/table/tbody/tr[2]/td/div[1]/div[1]/a/img');
+  const propertyImageLink = await (await el.getProperty('src')).jsonValue();
+
+  const property = {
+    name: propertyName,
+    price: propertyPrice,
+    type: propertyType,
+    link: propertyLink,
+    imageLink: propertyImageLink,
+  };
+
+  console.log(property);
 }
 
-scrapeProperties('https://www.home.co.uk/search/results.htm?location=islington&TOWN_SEARCH=1&high=500000&detached=true&semi=true&terraced=true&flat=true&sort=PRICE_DESC&inc_sold=0&showmap=0&loose=1');
+scrapeProperties('https://www.home.co.uk/search/results.htm?location=harrow&TOWN_SEARCH=1&high=500000');
