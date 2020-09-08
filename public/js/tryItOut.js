2f0fb629155df5919dfb45d7b4e2f44979b0afc8
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-09-08 20:11:48 +0100

    Returned API results to page, improved styling and changed API result section to be hidden by default

--- public/js/tryItOut.js
+++ public/js/tryItOut.js
@@ -4,8 +4,11 @@ function showInput() {
   let query = '';
 
   for (let j = 0; j < elements.childNodes.length; j++) {
+    // Only interested in INPUT items (numerical, checkboxes and slider) and location SELECT item
     if ((elements.childNodes[j].value)
         && (['SELECT', 'INPUT'].indexOf(elements.childNodes[j].tagName) >= 0)) {
+
+      // The checkbox state is returned through .checked rather than .value
       if (elements.childNodes[j].type === 'checkbox') {
         if (elements.childNodes[j].checked) {
           if (query) { query = `${query}&`; }
@@ -18,5 +21,14 @@ function showInput() {
     }
   }
 
-  document.getElementById('try-it-out-endpoint').innerHTML = `${document.URL}properties?${query}`;
+  const url = `${document.URL}properties?${query}`
+
+  // Return the API call so the user can utilise as necessary
+  document.getElementById('try-it-out-url').innerHTML = url;
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => document.getElementById('try-it-out-results').innerHTML = JSON.stringify(data.slice(1,3), undefined, 2));
+
+  document.getElementsByClassName('try-it-out-results-container')[0].style.display = 'block';
 }
