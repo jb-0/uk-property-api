4f99683c4b05c5d6e2384a884dd8a4b9c71fd5d9
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-03-14 16:26:51 +0000

    Enhanced styling

--- client/src/pages/home.styles.js
+++ client/src/pages/home.styles.js
@@ -9,5 +9,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.HeaderSection = void 0;
 var styled_components_1 = __importDefault(require("styled-components"));
-exports.HeaderSection = styled_components_1.default.section(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  /* background-color: red; */\n"], ["\n  /* background-color: red; */\n"])));
+exports.HeaderSection = styled_components_1.default.section(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  display: flex;\n  flex-direction: column;\n  flex-wrap: wrap;\n  justify-content: center;\n  align-items: center;\n  height: 400px;\n  width: 800px;\n  margin: auto;\n"], ["\n  display: flex;\n  flex-direction: column;\n  flex-wrap: wrap;\n  justify-content: center;\n  align-items: center;\n  height: 400px;\n  width: 800px;\n  margin: auto;\n"])));
 var templateObject_1;
--- client/src/pages/home.styles.tsx
+++ client/src/pages/home.styles.tsx
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
 export const HeaderSection = styled.section`
-  /* background-color: red; */
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+  width: 800px;
+  margin: auto;
 `;
