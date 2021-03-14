17eca994f6c030ef98c49afe512def76d5d91c03
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-03-14 13:00:13 +0000

    Added styled components and created home styles

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,9 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(require("react"));
-function Home() {
-    return (react_1.default.createElement("section", null,
+var home_styles_js_1 = require("./home.styles.js");
+var Home = function () {
+    return (react_1.default.createElement(home_styles_js_1.HeaderSection, null,
         react_1.default.createElement("h1", null, "UK Property API"),
         react_1.default.createElement("p", null, "Access details for almost half a million properties currently listed for sale on the UK property market")));
-}
+};
 exports.default = Home;
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { HeaderSection } from './home.styles.js'
 
-function Home(): JSX.Element {
+const Home = (): JSX.Element => {
   return (
-    <section>
+    <HeaderSection>
       <h1>UK Property API</h1>
       <p>
         Access details for almost half a million properties currently listed for
         sale on the UK property market
       </p>
-    </section>
+    </HeaderSection>
   );
-}
+};
 
 export default Home;
