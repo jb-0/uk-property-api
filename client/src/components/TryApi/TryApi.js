802a5e86308a35eda9d4f37d0018ff1197a7673b
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-03-18 17:17:21 +0000

    Introduced additional state and useEffect to fetch data from backend

--- client/src/components/TryApi/TryApi.js
+++ client/src/components/TryApi/TryApi.js
@@ -29,6 +29,42 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -42,9 +78,11 @@ var PropertyType_1 = __importDefault(require("./PropertyType"));
 var TryApi_styles_1 = require("./TryApi.styles");
 function TryApi() {
     var _a = react_1.useState(new interfaces_1.PropertyAttributes()), propertyAttributes = _a[0], setPropertyAttributes = _a[1];
-    function handleFormUpdates(event) {
+    var _b = react_1.useState(false), submittedForm = _b[0], setSubmittedForm = _b[1];
+    var _c = react_1.useState(), propertyData = _c[0], setPropertyData = _c[1];
+    function handleFormUpdates(e) {
         // Get the object key so we know which element needs updating
-        var targetPropertyType = event.target.id;
+        var targetPropertyType = e.target.id;
         // For boolean cases just use not to switch the value
         if (typeof propertyAttributes[targetPropertyType] === 'boolean') {
             setPropertyAttributes(function (prevValues) {
@@ -56,16 +94,56 @@ function TryApi() {
         else {
             setPropertyAttributes(function (prevValues) {
                 var _a;
-                return __assign(__assign({}, prevValues), (_a = {}, _a[targetPropertyType] = event.target.value, _a));
+                return __assign(__assign({}, prevValues), (_a = {}, _a[targetPropertyType] = e.target.value, _a));
             });
         }
     }
-    return (react_1.default.createElement(TryApi_styles_1.TryApiForm, null,
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        setSubmittedForm(true);
+    }
+    react_1.useEffect(function () {
+        function callPropertyAPI() {
+            return __awaiter(this, void 0, void 0, function () {
+                var query, response, _a;
+                return __generator(this, function (_b) {
+                    switch (_b.label) {
+                        case 0:
+                            query = Object.keys(propertyAttributes)
+                                .map(function (key) {
+                                return key + "=" + propertyAttributes[key];
+                            })
+                                .join('&');
+                            return [4 /*yield*/, fetch("http://localhost:8080/properties?" + query, {
+                                    method: 'GET',
+                                    headers: {
+                                        Accept: 'application/json',
+                                        'Content-Type': 'application/json',
+                                    },
+                                })];
+                        case 1:
+                            response = _b.sent();
+                            _a = setPropertyData;
+                            return [4 /*yield*/, response.json()];
+                        case 2:
+                            _a.apply(void 0, [_b.sent()]);
+                            return [2 /*return*/];
+                    }
+                });
+            });
+        }
+        if (submittedForm) {
+            callPropertyAPI();
+            setSubmittedForm(false);
+        }
+    });
+    return (react_1.default.createElement(TryApi_styles_1.TryApiForm, { onSubmit: handleFormSubmit },
         react_1.default.createElement("h1", null, "Try it out"),
         react_1.default.createElement("p", null, "Fill out the form below and click submit to try out the UK Property API"),
         react_1.default.createElement(Location_1.default, { propertyAttributes: propertyAttributes, handleFormUpdates: handleFormUpdates }),
         react_1.default.createElement(Price_1.default, { propertyAttributes: propertyAttributes, handleFormUpdates: handleFormUpdates }),
         react_1.default.createElement(Bedrooms_1.default, { propertyAttributes: propertyAttributes, handleFormUpdates: handleFormUpdates }),
-        react_1.default.createElement(PropertyType_1.default, { propertyAttributes: propertyAttributes, handleFormUpdates: handleFormUpdates })));
+        react_1.default.createElement(PropertyType_1.default, { propertyAttributes: propertyAttributes, handleFormUpdates: handleFormUpdates }),
+        react_1.default.createElement(TryApi_styles_1.SubmitButton, { type: 'submit' })));
 }
 exports.default = TryApi;
--- client/src/components/TryApi/TryApi.tsx
+++ client/src/components/TryApi/TryApi.tsx
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Bedrooms from './Bedrooms';
 import { PropertyAttributes } from './interfaces';
 import Location from './Location';
 import Price from './Price';
 import PropertyType from './PropertyType';
-import { TryApiForm } from './TryApi.styles';
+import { TryApiForm, SubmitButton } from './TryApi.styles';
 
 export default function TryApi(): JSX.Element {
   const [propertyAttributes, setPropertyAttributes] = useState(
     new PropertyAttributes()
   );
+  const [submittedForm, setSubmittedForm] = useState(false);
+  const [propertyData, setPropertyData] = useState();
 
   function handleFormUpdates(
-    event:
+    e:
       | React.ChangeEvent<HTMLInputElement>
       | React.ChangeEvent<HTMLSelectElement>
   ) {
     // Get the object key so we know which element needs updating
-    const targetPropertyType: string = event.target.id;
+    const targetPropertyType: string = e.target.id;
 
     // For boolean cases just use not to switch the value
     if (
@@ -39,14 +41,46 @@ export default function TryApi(): JSX.Element {
       setPropertyAttributes((prevValues) => {
         return {
           ...prevValues,
-          [targetPropertyType]: event.target.value,
+          [targetPropertyType]: e.target.value,
         };
       });
     }
   }
 
+  function handleFormSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setSubmittedForm(true);
+  }
+
+  useEffect(() => {
+    async function callPropertyAPI() {
+      const query = Object.keys(propertyAttributes)
+        .map(
+          (key) =>
+            `${key}=${propertyAttributes[key as keyof PropertyAttributes]}`
+        )
+        .join('&');
+      
+      const response = await fetch(`http://localhost:8080/properties?${query}`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      
+      setPropertyData(await response.json());
+    }
+
+    if (submittedForm) {
+      callPropertyAPI();
+      setSubmittedForm(false);
+    }
+  });
+
   return (
-    <TryApiForm>
+    <TryApiForm onSubmit={handleFormSubmit}>
       <h1>Try it out</h1>
       <p>
         Fill out the form below and click submit to try out the UK Property API
@@ -67,6 +101,8 @@ export default function TryApi(): JSX.Element {
         propertyAttributes={propertyAttributes}
         handleFormUpdates={handleFormUpdates}
       />
+
+      <SubmitButton type='submit' />
     </TryApiForm>
   );
 }
