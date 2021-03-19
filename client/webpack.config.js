0c11ea8a510e826755a102509a26ef78b4e9a9f1
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-03-19 07:24:42 +0000

    Added html loader to webpack and remove dist from gitignore

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -30,5 +31,9 @@ module.exports = {
     publicPath: "http://localhost:3000/dist/",
     hotOnly: true
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html"
+    })], 
 };
