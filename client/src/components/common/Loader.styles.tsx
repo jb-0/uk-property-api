76588c9a09788d6e16aba19d9d49ad869280ccc9
Jamie Barrett <timothybarrett@hotmail.co.uk>
2021-03-25 18:35:59 +0000

    Created base files for loader component

---
+++ client/src/components/common/Loader.styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const LoaderCircle = styled.div`
+  background-color: red;
+  border-radius: 20px;
+  height: 30px;
+  width: 30px;
+`;
+
+// export const LoaderCircles 
---
+++ client/src/components/common/Loader.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { LoaderCircle } from './Loader.styles';
+
+export default function Loader(): JSX.Element  {
+  return (
+    <LoaderCircle />
+  )
+}
